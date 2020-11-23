5bb835acef51f26cf79e25ae46c014a5b9cd4158
Yunus Emre - Mac <yunusemre@sariilanlar.com>
2020-11-23 21:13:36 +0300

    bug fixing in _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import "../styles/index.css";
 import React, { useEffect } from "react";
 import Head from "next/head";
 import LogRocket from "logrocket";
-// import setupLogRocketReact from "logrocket-react";
+import setupLogRocketReact from "logrocket-react";
 // import { hotjar } from "react-hotjar";
 
 /* eslint-disable react/prop-types,react/jsx-props-no-spreading */

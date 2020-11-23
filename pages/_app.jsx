7201ccbce5143013bc4900dd2b8f6c5a4dfd2a04
Yunus Emre - Mac <yunusemre@sariilanlar.com>
2020-11-23 21:08:20 +0300

    logrocket setup

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,17 @@
-import '../styles/index.css';
-import React from 'react';
-import Head from 'next/head';
-// import LogRocket from "logrocket";
+import "../styles/index.css";
+import React from "react";
+import Head from "next/head";
+import LogRocket from "logrocket";
 // import setupLogRocketReact from "logrocket-react";
 // import { hotjar } from "react-hotjar";
 
 /* eslint-disable react/prop-types,react/jsx-props-no-spreading */
 function MyApp({ Component, pageProps }) {
-  // useEffect(() => {
-  //   LogRocket.init("jqzjcy/sari-ilanlar");
-  //   setupLogRocketReact(LogRocket);
-  //   hotjar.initialize("2064026", "6");
-  // }, []);
+  useEffect(() => {
+    LogRocket.init("jqzjcy/disaricikmalimiyimcom");
+    setupLogRocketReact(LogRocket);
+    // hotjar.initialize("2064026", "6");
+  }, []);
   return (
     <div className="w-full h-full bg-red-500 relative">
       <Head>
@@ -26,14 +26,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 /* eslint-enable react/prop-types,react/jsx-props-no-spreading */
-export function reportWebVitals(metric) {
-  const body = JSON.stringify(metric);
-  const url = '/__appsignal-web-vitals';
-
-  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-
-  // eslint-disable-next-line
-  (navigator.sendBeacon && navigator.sendBeacon(url, body))
-    || fetch(url, { body, method: 'POST', keepalive: true });
-}
 export default MyApp;
